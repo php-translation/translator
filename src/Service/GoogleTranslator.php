63ebee95bc55e8d946ecd726c9a62cf74de04263
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-12-03 21:18:32 +0000

    Apply fixes from StyleCI

--- src/Service/GoogleTranslator.php
+++ src/Service/GoogleTranslator.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * This file is part of the PHP Translation package.
+ *
+ * (c) PHP Translation team <tobias.nyholm@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
 namespace Translation\Translator\Service;
 
 use Http\Client\HttpClient;
